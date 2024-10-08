c5ec8963da6dd7a83f9554a20b6006083e8f81d0
Sapna1921157 <sapnak7508@gmail.com>
2024-10-08 11:15:24 +0530

    uninstalling angular

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+// index.js
+const express = require('express');
+const app = express();
+
+// Define a simple route
+app.get('/', (req, res) => {
+  res.send('Hello, Express!');
+});
+
+// Start the server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
